3e098f247f58d2d20138c4ce28e3bb260745086d
梁弘毅 <55395193+tesseract4d@users.noreply.github.com>
2024-06-05 08:59:22 +0800

    merge config class

--- src/main/java/com/myname/mymodid/MyMod.java
+++ src/main/java/com/myname/mymodid/MyMod.java
@@ -2,11 +2,24 @@ package com.myname.mymodid;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.common.config.Configuration;
+
+import java.io.File;
 
 @Mod(modid = "mymodid", acceptedMinecraftVersions = "[1.7.10]")
 public class MyMod {
+    public static String greeting;
+
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent e) {
-        Config.sync(e.getSuggestedConfigurationFile());
+        syncConfig(e.getSuggestedConfigurationFile());
+    }
+
+    public static void syncConfig(File f) {
+        Configuration configuration = new Configuration(f);
+        greeting = configuration.getString("greeting", Configuration.CATEGORY_GENERAL, "Hello World", "How shall I greet?");
+        if (configuration.hasChanged()) {
+            configuration.save();
+        }
     }
 }
